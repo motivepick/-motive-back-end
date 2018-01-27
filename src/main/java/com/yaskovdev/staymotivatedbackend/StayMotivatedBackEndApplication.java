0f7d301cd41a6e637d91b550590b3f882b9754b2
yaskovdev <yaskovdev@gmail.com>
2018-01-27 18:04:54 +0200

    Create initial project structure.

---
+++ src/main/java/com/yaskovdev/staymotivatedbackend/StayMotivatedBackEndApplication.java
@@ -0,0 +1,12 @@
+package com.yaskovdev.staymotivatedbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayMotivatedBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StayMotivatedBackEndApplication.class, args);
+	}
+}
---
+++ src/test/java/com/yaskovdev/staymotivatedbackend/StayMotivatedBackEndApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yaskovdev.staymotivatedbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StayMotivatedBackEndApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
