98578a13b37ec8b3166a108c6413abb3bdf5f9d8
yaskovdev <yaskovdev@gmail.com>
2018-01-28 16:16:20 +0200

    Allow all origins for the development purposes.

--- src/main/java/com/yaskovdev/motivated/stay/StayMotivatedBackEndApplication.java
+++ src/main/java/com/yaskovdev/motivated/stay/StayMotivatedBackEndApplication.java
@@ -2,11 +2,26 @@ package com.yaskovdev.motivated.stay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class StayMotivatedBackEndApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StayMotivatedBackEndApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StayMotivatedBackEndApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
 }
