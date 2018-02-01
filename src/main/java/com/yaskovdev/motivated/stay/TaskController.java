1fd21517f14021e724d45cd09406cea6642118b1
Jelena <jelena.shaipuka@gmail.com>
2018-02-01 16:27:43 +0200

    Revert "Revert "Linking to mongodb""
    This reverts commit d1309a3d95cca6707fa935ad9474e9d1acbd316c.

---
+++ src/main/java/com/yaskovdev/motivated/stay/TaskController.java
@@ -0,0 +1,63 @@
+package com.yaskovdev.motivated.stay;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class TaskController {
+
+    private final TaskRepository repo;
+
+    @Autowired
+    public TaskController(TaskRepository repo) {
+        this.repo = repo;
+    }
+
+    @GetMapping("/tasks")
+    ResponseEntity listTasks() {
+        return new ResponseEntity(repo.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/tasks/{id}")
+    ResponseEntity getTask(@PathVariable("id") final String taskId) {
+        if (!repo.exists(taskId)) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity(repo.findOne(taskId), HttpStatus.OK);
+    }
+
+    @PostMapping("/tasks")
+    ResponseEntity createTask(@RequestBody final Task task) {
+        // TODO: add validation
+        repo.insert(task);
+
+        return new ResponseEntity(task, HttpStatus.OK);
+    }
+
+    @PutMapping("/tasks/{id}")
+    ResponseEntity updateTask(@PathVariable("id") final String taskId, @RequestBody final Task task) {
+        // TODO: add validation
+
+        if (!repo.exists(taskId)) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+
+        repo.save(task);
+
+        return new ResponseEntity(task, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/tasks/{id}")
+    ResponseEntity deleteTask(@PathVariable("id") final String taskId) {
+        if (!repo.exists(taskId)) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+
+        repo.delete(taskId);
+
+        return new ResponseEntity(HttpStatus.OK);
+    }
+}
