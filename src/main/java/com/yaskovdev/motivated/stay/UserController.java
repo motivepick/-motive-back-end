0a7b261b291755ebdb5c93198785b68de0811830
yaskovdev <yaskovdev@gmail.com>
2018-06-09 22:17:15 +0300

    Introduce a concept of a user.

--- src/main/java/com/yaskovdev/motivated/stay/UserController.java
+++ src/main/java/com/yaskovdev/motivated/stay/UserController.java
@@ -4,7 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +19,7 @@ public class UserController {
         this.repository = repository;
     }
 
-    @PutMapping("/{id}")
+    @PostMapping("/{id}") // TODO: can I use PUT
     public ResponseEntity<User> create(@PathVariable("id") final String id, @RequestBody final User user) {
         user.setId(id);
         return ok(repository.insert(user));
