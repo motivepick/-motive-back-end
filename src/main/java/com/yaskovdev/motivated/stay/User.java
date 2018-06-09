9f62ba5e24d2f56e479def6dfb2ae06159bbc3aa
yaskovdev <yaskovdev@gmail.com>
2018-06-09 22:03:54 +0300

    Introduce a concept of a user.

---
+++ src/main/java/com/yaskovdev/motivated/stay/User.java
@@ -0,0 +1,54 @@
+package com.yaskovdev.motivated.stay;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Objects;
+
+@Document
+public class User {
+
+    @Id
+    private String id;
+
+    private String name;
+
+    private String token;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/com/yaskovdev/motivated/stay/UserController.java
@@ -0,0 +1,38 @@
+package com.yaskovdev.motivated.stay;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.ResponseEntity.ok;
+
+@RestController("/users")
+public class UserController {
+
+    private final UserRepository repository;
+
+    public UserController(final UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> create(@PathVariable("id") final String id, @RequestBody final User user) {
+        user.setId(id);
+        return ok(repository.insert(user));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> read(@PathVariable("id") final String id) {
+        return ok(repository.findOne(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<User> delete(@PathVariable("id") final String id) {
+        repository.delete(id);
+        return ok().build();
+    }
+}
