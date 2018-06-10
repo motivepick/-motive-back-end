cbc2d5d2f3c602e3a91ea3bed26b17043ce5056d
yaskovdev <yaskovdev@gmail.com>
2018-06-10 09:23:59 +0300

    Introduce a concept of a user.

--- src/main/java/com/yaskovdev/motivated/stay/UserController.java
+++ src/main/java/com/yaskovdev/motivated/stay/UserController.java
@@ -1,7 +1,6 @@
 package com.yaskovdev.motivated.stay;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,7 +37,7 @@ public class UserController {
         return ok(repository.findAll());
     }
 
-    @DeleteMapping("/{id}")
+    @PostMapping("/{id}/deletion")
     public ResponseEntity<User> delete(@PathVariable("id") final String id) {
         repository.delete(id);
         return ok().build();
