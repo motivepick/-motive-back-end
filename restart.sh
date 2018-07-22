b8a91def85cd4f3943de120b71913b3eb12476de
yaskovdev <yaskovdev@gmail.com>
2018-07-22 20:57:13 +0300

    Switch to another database host.

--- restart.sh
+++ restart.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export SPRING_DATA_MONGODB_HOST=91.92.128.214
+export SPRING_DATA_MONGODB_HOST=167.99.40.5
 export SPRING_DATA_MONGODB_PORT=27017
 
 PID_FILE=./application.pid
