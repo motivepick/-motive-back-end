eda83a01c0f8f1c1d035cbf176a40af1288aae43
rustock0 <emironen0@gmail.com>
2018-08-09 21:25:07 +0300

    Closes #4; Task model has been extended by subtasks

--- src/main/kotlin/org/motivepick/domain/Task.kt
+++ src/main/kotlin/org/motivepick/domain/Task.kt
@@ -1,6 +1,7 @@
 package org.motivepick.domain
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.DBRef
 import org.springframework.data.mongodb.core.mapping.Document
 import java.time.LocalDateTime
 import javax.validation.constraints.NotBlank
@@ -17,4 +18,7 @@ class Task {
     var instantOfCreation: LocalDateTime? = null
     var dueDate: LocalDateTime? = null
     var closed: Boolean = false
+
+    @DBRef(lazy = true)
+    var subtasks: MutableList<Task> = ArrayList()
 }
