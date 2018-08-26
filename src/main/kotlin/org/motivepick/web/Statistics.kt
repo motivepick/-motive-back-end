edca3bae8e6e26df37730a50222d285be1efe34a
yaskovdev <yaskovdev@gmail.com>
2018-08-26 21:51:01 +0300

    Add statistics.

--- src/main/kotlin/org/motivepick/web/Statistics.kt
+++ src/main/kotlin/org/motivepick/web/Statistics.kt
@@ -3,4 +3,4 @@ package org.motivepick.web
 import java.time.LocalDateTime
 
 class Statistics(val totalOpenTasks: Int, val totalClosedTasks: Int, val totalOverdueTasks: Int, val openTasksDueToday: Int,
-        val closedTasksDueToday: Int, val nextDueDate: LocalDateTime)
+        val closedTasksDueToday: Int, val nextDueDate: LocalDateTime?)
