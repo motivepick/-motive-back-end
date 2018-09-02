b8c4649fb48237a3ff75ecf6cf56bcb943c7e21d
rustock0 <emironen0@gmail.com>
2018-09-02 20:57:58 +0300

    restart.sh has been updated

--- restart.sh
+++ restart.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
-export SPRING_DATA_MONGODB_HOST=127.0.0.1
-export SPRING_DATA_MONGODB_PORT=27017
-
 PID_FILE=./application.pid
+JAR_PATH=target/motive-back-end-0.0.1-SNAPSHOT.jar
 
 if [ -f "$PID_FILE" ]
 then
     kill $(cat ${PID_FILE})
 fi
 
-nohup ./mvnw spring-boot:run >/dev/null 2>&1 &
+nohup java -Dspring.config.location=file:./../secure.yml,src/main/resources/application.yml -jar -Dspring.profiles.active=prod $JAR_PATH > /dev/null 2>&1 &
