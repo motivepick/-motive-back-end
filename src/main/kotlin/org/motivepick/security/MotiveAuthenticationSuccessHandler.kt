540586e2f8982d82d08239dcc8b272d9b0b0cf25
rustock0 <emironen0@gmail.com>
2018-09-02 19:30:01 +0300

    SavedRequestAwareAuthenticationSuccessHandler was used instead of SimpleUrlAuthenticationSuccessHandler

--- src/main/kotlin/org/motivepick/security/MotiveAuthenticationSuccessHandler.kt
+++ src/main/kotlin/org/motivepick/security/MotiveAuthenticationSuccessHandler.kt
@@ -5,7 +5,7 @@ import org.motivepick.repository.UserRepository
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.security.core.Authentication
 import org.springframework.security.oauth2.provider.OAuth2Authentication
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler
 import org.springframework.stereotype.Component
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
@@ -15,7 +15,7 @@ class MotiveAuthenticationSuccessHandler(@Value("\${authentication.success.url}"
                                          private val authenticationSuccessUrl: String,
 
                                          private val userRepo: UserRepository)
-    : SimpleUrlAuthenticationSuccessHandler() {
+    : SavedRequestAwareAuthenticationSuccessHandler() {
 
     init {
         isAlwaysUseDefaultTargetUrl = true
