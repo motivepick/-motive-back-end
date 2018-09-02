43dd96e98e11a3af5784bd167f714441b3bf97e8
rustock0 <emironen0@gmail.com>
2018-09-02 21:34:35 +0300

    restart.sh has been updated

--- restart.sh
+++ restart.sh
@@ -2,10 +2,11 @@
 
 PID_FILE=./application.pid
 JAR_PATH=target/motive-back-end-0.0.1-SNAPSHOT.jar
+SECURE_PATH=../secure.yml
 
 if [ -f "$PID_FILE" ]
 then
     kill $(cat ${PID_FILE})
 fi
 
-nohup java -Dspring.config.location=file:./../secure.yml,src/main/resources/application.yml -jar -Dspring.profiles.active=prod $JAR_PATH > /dev/null 2>&1 &
+nohup java -jar -Dspring.profiles.active=prod $JAR_PATH --spring.config.location=classpath:application.yml,classpath:application-prod.yml,file:$SECURE_PATH > /dev/null 2>&1 &
