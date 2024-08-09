c6932828d680ae406c7aa070c6d22f1fdb8fd854
Sergey Yaskov <yaskovdev@gmail.com>
2024-08-09 18:19:23 +0300

    Migrate a couple files.

--- src/main/java/org/motivepick/service/AbstractOauth2LoginService.java
+++ src/main/java/org/motivepick/service/AbstractOauth2LoginService.java
@@ -1,68 +1,90 @@
-package org.motivepick.service
+package org.motivepick.service;
 
-import org.motivepick.config.CookieFactory
-import org.motivepick.config.Oauth2Config
-import org.motivepick.config.ServerConfig
-import org.motivepick.domain.entity.LoginStateEntity
-import org.motivepick.repository.LoginStateRepository
-import org.motivepick.security.AbstractTokenGenerator
-import org.springframework.security.authentication.AuthenticationServiceException
-import org.springframework.transaction.annotation.Transactional
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder
-import org.springframework.web.util.UriComponentsBuilder
-import java.util.*
-import javax.servlet.http.HttpServletRequest
-import javax.servlet.http.HttpServletResponse
+import com.google.common.base.Charsets;
+import org.jetbrains.annotations.NotNull;
+import org.motivepick.config.CookieFactory;
+import org.motivepick.config.Oauth2Config;
+import org.motivepick.config.ServerConfig;
+import org.motivepick.domain.entity.LoginStateEntity;
+import org.motivepick.repository.LoginStateRepository;
+import org.motivepick.security.AbstractTokenGenerator;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
-internal abstract class AbstractOauth2LoginService(
-    private val config: Oauth2Config,
-    private val tokenGenerator: AbstractTokenGenerator,
-    private val serverConfig: ServerConfig,
-    private val cookieFactory: CookieFactory,
-    private val loginStateRepository: LoginStateRepository
-) : Oauth2LoginService {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Base64;
+import java.util.Locale;
+import java.util.UUID;
 
-    @Transactional
-    override fun login(request: HttpServletRequest, response: HttpServletResponse) {
-        val uuid = UUID.randomUUID().toString()
-        val state = Base64.getEncoder().encodeToString(uuid.toByteArray())
-        val mobile = request.getParameter("mobile") != null
-        loginStateRepository.save(LoginStateEntity(uuid, mobile))
+abstract class AbstractOauth2LoginService implements Oauth2LoginService {
 
-        val redirectUrl = ServletUriComponentsBuilder.fromCurrentRequestUri()
-            .scheme(if (serverConfig.enforceHttpsForOauth) "https" else "http")
-            .path("/callback")
-            .toUriString()
+    private final Oauth2Config config;
+    private final AbstractTokenGenerator tokenGenerator;
+    private final ServerConfig serverConfig;
+    private final CookieFactory cookieFactory;
+    private final LoginStateRepository loginStateRepository;
 
-        val authorizationUri = UriComponentsBuilder.fromUriString(config.userAuthorizationUri)
-            .queryParam("client_id", config.clientId)
-            .queryParam("redirect_uri", redirectUrl)
-            .queryParam("state", state)
-            .toUriString()
-
-        response.sendRedirect(authorizationUri)
+    public AbstractOauth2LoginService(Oauth2Config config, AbstractTokenGenerator tokenGenerator, ServerConfig serverConfig,
+        CookieFactory cookieFactory, LoginStateRepository loginStateRepository) {
+        this.config = config;
+        this.tokenGenerator = tokenGenerator;
+        this.serverConfig = serverConfig;
+        this.cookieFactory = cookieFactory;
+        this.loginStateRepository = loginStateRepository;
     }
 
-    @Transactional
-    override fun loginCallback(request: HttpServletRequest, response: HttpServletResponse) {
-        val locale = request.locale
-        val code = request.getParameter("code")
-        val stateUuid = String(Base64.getDecoder().decode(request.getParameter("state")))
-        val state = loginStateRepository.findByStateUuid(stateUuid)
-            .orElseThrow { AuthenticationServiceException("Provided state is incorrect or expired") }
+    @Override
+    public void login(@NotNull HttpServletRequest request, @NotNull HttpServletResponse response) {
+        var uuid = UUID.randomUUID().toString();
+        var state = Base64.getEncoder().encodeToString(uuid.getBytes(Charsets.UTF_8));
+        boolean mobile = request.getParameter("mobile") != null;
+        loginStateRepository.save(new LoginStateEntity(uuid, mobile));
 
-        val redirectUrl = ServletUriComponentsBuilder.fromCurrentRequestUri()
-            .scheme(if (serverConfig.enforceHttpsForOauth) "https" else "http")
-            .toUriString()
-        val jwtToken = tokenGenerator.generateJwtToken(code, redirectUrl, locale.language)
+        var redirectUrl = ServletUriComponentsBuilder.fromCurrentRequestUri()
+            .scheme(serverConfig.getEnforceHttpsForOauth() ? "https" : "http")
+            .path("/callback")
+            .toUriString();
 
-        if (state.mobile) {
-            response.sendRedirect(serverConfig.authenticationSuccessUrlMobile + jwtToken)
+        var authorizationUri = UriComponentsBuilder.fromUriString(config.getUserAuthorizationUri())
+            .queryParam("client_id", config.getClientId())
+            .queryParam("redirect_uri", redirectUrl)
+            .queryParam("state", state)
+            .toUriString();
+
+        sendRedirect(response, authorizationUri);
+    }
+
+    @Override
+    public void loginCallback(@NotNull HttpServletRequest request, @NotNull HttpServletResponse response) {
+        var locale = request.getLocale();
+        var code = request.getParameter("code");
+        var stateUuid = new String(Base64.getDecoder().decode(request.getParameter("state")));
+        var state = loginStateRepository.findByStateUuid(stateUuid)
+            .orElseThrow(() -> new AuthenticationServiceException("Provided state is incorrect or expired"));
+
+        var redirectUrl = ServletUriComponentsBuilder.fromCurrentRequestUri()
+            .scheme(serverConfig.getEnforceHttpsForOauth() ? "https" : "http")
+            .toUriString();
+        var jwtToken = tokenGenerator.generateJwtToken(code, redirectUrl, locale.getLanguage());
+
+        if (state.getMobile()) {
+            sendRedirect(response, serverConfig.getAuthenticationSuccessUrlMobile() + jwtToken);
         } else {
-            response.addCookie(cookieFactory.cookie(jwtToken))
-            response.sendRedirect(serverConfig.authenticationSuccessUrlWeb)
+            response.addCookie(cookieFactory.cookie(jwtToken));
+            sendRedirect(response, serverConfig.getAuthenticationSuccessUrlWeb());
         }
 
-        loginStateRepository.deleteByStateUuid(stateUuid)
+        loginStateRepository.deleteByStateUuid(stateUuid);
+    }
+
+    private static void sendRedirect(@NotNull HttpServletResponse response, String authorizationUri) {
+        try {
+            response.sendRedirect(authorizationUri);
+        } catch (IOException e) {
+            throw new RuntimeException("Unexpected exception happened when trying to send redirect", e);
+        }
     }
 }
--- src/main/java/org/motivepick/web/TemporaryAccountController.java
+++ src/main/java/org/motivepick/web/TemporaryAccountController.java
@@ -1,31 +1,53 @@
-package org.motivepick.web
+package org.motivepick.web;
 
-import org.motivepick.config.CookieFactory
-import org.motivepick.config.ServerConfig
-import org.motivepick.security.JwtTokenService
-import org.motivepick.security.Profile
-import org.motivepick.service.UserService
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
-import java.util.*
-import javax.servlet.http.HttpServletRequest
-import javax.servlet.http.HttpServletResponse
+import org.jetbrains.annotations.NotNull;
+import org.motivepick.config.CookieFactory;
+import org.motivepick.config.ServerConfig;
+import org.motivepick.security.JwtTokenService;
+import org.motivepick.security.Profile;
+import org.motivepick.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.UUID;
 
 @RestController
-internal class TemporaryAccountController(private val config: ServerConfig, private val tokenService: JwtTokenService,
-        private val userService: UserService, private val cookieFactory: CookieFactory) {
+public class TemporaryAccountController {
+
+    private final ServerConfig config;
+    private final JwtTokenService tokenService;
+    private final UserService userService;
+    private final CookieFactory cookieFactory;
+
+    public TemporaryAccountController(ServerConfig config, JwtTokenService tokenService, UserService userService, CookieFactory cookieFactory) {
+        this.config = config;
+        this.tokenService = tokenService;
+        this.userService = userService;
+        this.cookieFactory = cookieFactory;
+    }
 
     @GetMapping("/temporary/login")
-    fun login(request: HttpServletRequest, response: HttpServletResponse) {
-        val locale = request.locale
-        val temporaryAccountId = UUID.randomUUID().toString()
-        userService.createUserWithTasksIfNotExists(Profile(temporaryAccountId, "", true), locale.language)
-        val token = tokenService.createAccessJwtToken(temporaryAccountId)
+    void login(HttpServletRequest request, HttpServletResponse response) {
+        var locale = request.getLocale();
+        var temporaryAccountId = UUID.randomUUID().toString();
+        userService.createUserWithTasksIfNotExists(new Profile(temporaryAccountId, "", true), locale.getLanguage());
+        var token = tokenService.createAccessJwtToken(temporaryAccountId);
         if (request.getParameter("mobile") == null) {
-            response.addCookie(cookieFactory.cookie(token))
-            response.sendRedirect(config.authenticationSuccessUrlWeb)
+            response.addCookie(cookieFactory.cookie(token));
+            sendRedirect(response, config.getAuthenticationSuccessUrlWeb());
         } else {
-            response.sendRedirect(config.authenticationSuccessUrlMobile + token)
+            sendRedirect(response, config.getAuthenticationSuccessUrlMobile() + token);
+        }
+    }
+
+    private static void sendRedirect(@NotNull HttpServletResponse response, String authorizationUri) {
+        try {
+            response.sendRedirect(authorizationUri);
+        } catch (IOException e) {
+            throw new RuntimeException("Unexpected exception happened when trying to send redirect", e);
         }
     }
 }
