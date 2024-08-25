ea2b34670c6e777ebeafd035e63faf839dd36311
Sergey Yaskov <yaskovdev@gmail.com>
2024-08-25 14:10:42 +0300

    Fix the `timeZone` param parsing.

--- src/main/kotlin/org/motivepick/web/ScheduleController.kt
+++ src/main/kotlin/org/motivepick/web/ScheduleController.kt
@@ -5,6 +5,7 @@ import org.motivepick.service.TaskService
 import org.springframework.http.ResponseEntity
 import org.springframework.http.ResponseEntity.ok
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.time.ZoneId
 
@@ -12,5 +13,6 @@ import java.time.ZoneId
 internal class ScheduleController(private val taskService: TaskService) {
 
     @GetMapping("/schedule")
-    fun schedule(timeZone: ZoneId?): ResponseEntity<ScheduleView> = ok(taskService.findScheduleForCurrentUser(timeZone ?: ZoneId.of("UTC")))
+    fun schedule(@RequestParam(name = "timeZone", defaultValue = "UTC") timeZone: String): ResponseEntity<ScheduleView> =
+        ok(taskService.findScheduleForCurrentUser(ZoneId.of(timeZone)))
 }
