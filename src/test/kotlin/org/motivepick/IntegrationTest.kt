5dc0ef5229e7b29876fb0706d0025c998b070878
Sergey Yaskov <yaskovdev@gmail.com>
2024-11-04 22:46:26 +0200

    Fix the tests.

--- src/test/kotlin/org/motivepick/IntegrationTest.kt
+++ src/test/kotlin/org/motivepick/IntegrationTest.kt
@@ -6,7 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.TestExecutionListeners
 import org.springframework.test.context.bean.override.mockito.MockitoResetTestExecutionListener
-import org.springframework.test.context.bean.override.mockito.MockitoTestExecutionListener
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener
 import org.springframework.test.context.support.DirtiesContextTestExecutionListener
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener
@@ -21,7 +20,6 @@ import org.springframework.test.context.transaction.TransactionalTestExecutionLi
     DependencyInjectionTestExecutionListener::class,
     DirtiesContextTestExecutionListener::class,
     TransactionalTestExecutionListener::class,
-    MockitoTestExecutionListener::class,
     OAuth2TestExecutionListener::class
 )
 internal annotation class IntegrationTest(val userAccountId: Long)
